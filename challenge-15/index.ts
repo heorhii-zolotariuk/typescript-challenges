477558727d584e410cbdfc33eae2ba320135d74d
heorhii.zolotariuk <heorhii.zolotariuk@gmail.com>
2025-03-01 10:51:04 +0200

    Challenge 15

---
+++ challenge-15/index.ts
@@ -0,0 +1,20 @@
+/*
+  ### Question
+
+  Implement the generic version of ```Array.push```
+
+  For example:
+
+  ```typescript
+  type Result = Push<[1, 2], '3'> // [1, 2, '3']
+  ```
+*/
+import type { Equal, Expect } from '../utils'
+
+type Push<T extends unknown[], U> = [...T, U];
+
+type cases = [
+    Expect<Equal<Push<[], 1>, [1]>>,
+    Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
+    Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
+]
