bf5116bab8a9892e1f0aebf978e20cf36cf895ab
heorhii.zolotariuk <heorhii.zolotariuk@gmail.com>
2025-03-02 12:17:57 +0200

    Challenge 20

---
+++ challenge-20/index.ts
@@ -0,0 +1,62 @@
+/*
+  ### Question
+
+  Implement a generic `MyReadonly2<T, K>` which takes two type argument `T` and `K`.
+
+  `K` specify the set of properties of `T` that should set to Readonly. When `K` is not provided, it should make all properties readonly just like the normal `Readonly<T>`.
+
+  For example
+
+  ```ts
+  interface Todo {
+    title: string
+    description: string
+    completed: boolean
+  }
+
+  const todo: MyReadonly2<Todo, 'title' | 'description'> = {
+    title: "Hey",
+    description: "foobar",
+    completed: false,
+  }
+
+  todo.title = "Hello" // Error: cannot reassign a readonly property
+  todo.description = "barFoo" // Error: cannot reassign a readonly property
+  todo.completed = true // OK
+  ```
+*/
+import type { Alike, Expect } from '../utils';
+
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+    readonly [TProp in keyof T as TProp extends K ? TProp : never]: T[TProp]
+} & {
+    [TProp in keyof T as TProp extends K ? never : TProp]: T[TProp]
+}
+
+type cases = [
+    Expect<Alike<MyReadonly2<Todo1>, Readonly<Todo1>>>,
+    Expect<Alike<MyReadonly2<Todo1, 'title' | 'description'>, Expected>>,
+    Expect<Alike<MyReadonly2<Todo2, 'title' | 'description'>, Expected>>,
+    Expect<Alike<MyReadonly2<Todo2, 'description' >, Expected>>,
+]
+
+// @ts-expect-error
+type error = MyReadonly2<Todo1, 'title' | 'invalid'>
+
+interface Todo1 {
+    title: string
+    description?: string
+    completed: boolean
+}
+
+interface Todo2 {
+    readonly title: string
+    description?: string
+    completed: boolean
+}
+
+interface Expected {
+    readonly title: string
+    readonly description?: string
+    completed: boolean
+}
--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,3 @@
 export type Equal<T, U> = (<G>() => G extends T ? 1 : 2) extends (<G>() => G extends U ? 1 : 2) ? true : false;
 export type Expect<T extends true> = T;
+export type Alike<T, U> = T extends U ? (U extends T ? true : false) : false;
