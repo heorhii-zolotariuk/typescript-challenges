190b3a2a3e51a41b6c06fce7b2b998642f1a258c
heorhii.zolotariuk <heorhii.zolotariuk@gmail.com>
2025-03-05 22:41:35 +0200

    Challenge 24

---
+++ challenge-24/index.ts
@@ -0,0 +1,27 @@
+/*
+  ### Question
+
+  > TypeScript 4.0 is recommended in this challenge
+
+  Implement a generic `Pop<T>` that takes an Array `T` and returns an Array without it's last element.
+
+  For example
+
+  ```ts
+  type arr1 = ['a', 'b', 'c', 'd']
+  type arr2 = [3, 2, 1]
+
+  type re1 = Pop<arr1> // expected to be ['a', 'b', 'c']
+  type re2 = Pop<arr2> // expected to be [3, 2]
+  ```
+*/
+import type { Equal, Expect } from '../utils'
+
+type Pop<T extends any[]> = T extends [...infer S, infer _] ? S : T;
+
+
+type cases = [
+    Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
+    Expect<Equal<Pop<['a', 'b', 'c', 'd']>, ['a', 'b', 'c']>>,
+    Expect<Equal<Pop<[]>, []>>,
+]
